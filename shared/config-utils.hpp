4c36cba6da2eee69232a3b96db1a1ad31e8b55ac
darknight1050 <darkdeveloping1050@gmail.com>
2023-11-26 14:22:45 +0100

    Update version

--- shared/config-utils.hpp
+++ shared/config-utils.hpp
@@ -49,7 +49,7 @@ ConfigUtils::ConfigValue<type> name = {&this->Save, &__##name, jsonName, def __V
 namespace ConfigUtils {
 
     inline Logger& getLogger() {
-        static auto logger = new Logger(modloader::ModInfo{"config-utils", "1.1.0", 0});
+        static auto logger = new Logger(modloader::ModInfo{"config-utils", "1.2.0", 0});
         return *logger;
     }
 
